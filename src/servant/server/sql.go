147877e06a334cffd999096d292bc4d8f06bfbf0
Xie Zhenye <xiezhenye@gmail.com>
2016-04-12 00:53:25 +0800

    use ErrorEnd

--- src/servant/server/sql.go
+++ src/servant/server/sql.go
@@ -19,7 +19,7 @@ func NewDatabaseServer(sess *Session) Handler {
 	}
 }
 
-func (self *DatabaseServer) findDatabaseQueryConfigByPath(path string) (*conf.Database, *conf.Query) {
+func (self *DatabaseServer) findDatabaseQueryConfig() (*conf.Database, *conf.Query) {
 	dbConf, ok := self.config.Databases[self.group]
 	if !ok {
 		return nil, nil
@@ -39,22 +39,19 @@ func (self *DatabaseServer) serve() {
 		self.ErrorEnd(http.StatusMethodNotAllowed, "not allow method: %s", method)
 		return
 	}
-	dbConf, queryConf := self.findDatabaseQueryConfigByPath(urlPath)
+	dbConf, queryConf := self.findDatabaseQueryConfig()
 	if dbConf == nil {
 		self.ErrorEnd(http.StatusNotFound, "database %s not found", urlPath)
 		return
 	}
 	if queryConf == nil {
-		self.warn("database", "- query %s not found", urlPath)
-		self.resp.WriteHeader(http.StatusNotFound)
+		self.ErrorEnd(http.StatusNotFound, "query %s not found", urlPath)
 		return
 	}
 
 	db, err := sql.Open(dbConf.Driver, dbConf.Dsn)
 	if err != nil {
-		self.warn("database", "- driver init failed")
-		self.resp.Header().Set(ServantErrHeader, err.Error())
-		self.resp.WriteHeader(http.StatusInternalServerError)
+		self.ErrorEnd(http.StatusInternalServerError, "driver init failed")
 		return
 	}
 	defer db.Close()
@@ -64,18 +61,14 @@ func (self *DatabaseServer) serve() {
 
 		result, err := dbQuery(db, sql, params)
 		if err != nil {
-			self.warn("database", "- query %s failed: %s", sql, err)
-			self.resp.Header().Set(ServantErrHeader, err.Error())
-			self.resp.WriteHeader(http.StatusInternalServerError)
+			self.ErrorEnd(http.StatusInternalServerError, "query %s failed: %s", sql, err)
 			return
 		}
 		data = append(data, result)
 	}
 	buf, err := json.Marshal(data)
 	if err != nil {
-		self.warn("database", "- json marshal failed: %s", err)
-		self.resp.Header().Set(ServantErrHeader, err.Error())
-		self.resp.WriteHeader(http.StatusInternalServerError)
+		self.ErrorEnd(http.StatusInternalServerError, "json marshal failed: %s", err)
 		return
 	}
 	self.resp.Write(buf)
